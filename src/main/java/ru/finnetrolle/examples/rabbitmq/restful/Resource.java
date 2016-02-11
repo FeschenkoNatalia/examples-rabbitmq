6ffa08f8a7256dc3fbc46631f4fdbb06ca5a4f8e
finnetrolle <maxsyachin@gmail.com>
2016-02-12 01:00:21 +0300

    fix

--- src/main/java/ru/finnetrolle/examples/rabbitmq/restful/Resource.java
+++ src/main/java/ru/finnetrolle/examples/rabbitmq/restful/Resource.java
@@ -1,14 +1,12 @@
 package ru.finnetrolle.examples.rabbitmq.restful;
 
+
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ru.finnetrolle.examples.rabbitmq.model.ResponseDto;
 import ru.finnetrolle.examples.rabbitmq.model.UserDto;
 
@@ -26,10 +24,17 @@ public class Resource {
     @Value("${rabbit.queue.test}")
     private String queueName;
 
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public class ResourceNotFoundException extends RuntimeException {}
+
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody
     ResponseDto digest(@RequestBody UserDto user) {
-        ResponseDto dto = (ResponseDto) template.convertSendAndReceive(queueName, user);
+        ResponseDto dto = (ResponseDto)
+                template.convertSendAndReceive(queueName, user);
+        if (dto == null) {
+            throw new ResourceNotFoundException();
+        }
         return dto;
     }
 
